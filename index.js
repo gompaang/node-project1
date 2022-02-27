0a947b49430a53fc5148d571e1ac4700c4ce93e8
hyejin <hjhur004@gmail.com>
2022-02-27 20:48:34 +0900

    몽고디비 관련 코드 보안문제로 인해 gitignore 파일에 넣음

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+const {User}= require("./models/User");
+const bodyParser = require('body-parser');
+const config = require('./config/key');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
 const mongoose = require('mongoose');
-mongoose.connect('mongodb+srv://hyejin:0427@project1.dnijl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI,{
     useNewUrlParser: true, 
     useUnifiedTopology: true, 
     // useCreateIndex: true, 
@@ -13,6 +20,16 @@ mongoose.connect('mongodb+srv://hyejin:0427@project1.dnijl.mongodb.net/myFirstDa
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
+})
+
+app.post('/register',(req,res)=>{
+    const user = new User(req.body);
+    user.save((err,userInfo)=>{ //save 는 몽고디비에 있는 함수   
+        if(err) return res.json({success:false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
 })
 
 app.listen(port, () => {
